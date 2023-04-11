9fef08cc902b10deffcc97e0793e3d5177dc585c
Felipe <120607893+f95f@users.noreply.github.com>
2023-04-11 16:48:22 -0300

    started working on scripts

---
+++ scripts/base.js
@@ -0,0 +1,26 @@
+let loadGame = function(operation){
+
+    switch(operation){
+
+        case 'add':
+            alert(operation);
+            window.location.href = "game.html";
+        break;
+            
+        case 'sub':
+                alert(operation);
+                // do shit
+        break;
+                
+        case 'mult':
+                    alert(operation);
+                    
+        break;
+                    
+        case 'div':
+            alert(operation);
+            // do shit
+        break;
+    }
+
+}
