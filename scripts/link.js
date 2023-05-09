b968680ad76e65bc5ca48101064559c7562e4f02
f95f <95felipefernandes@gmail.com>
2023-05-09 12:25:19 -0300

    a

--- scripts/link.js
+++ scripts/link.js
@@ -1,11 +1,32 @@
-
 let operation = sessionStorage.getItem("gameMode");
+let operationSymbol = '';
 let title = "";
+
+let difficulty = 1;
+let term1 = document.getElementById("term-1");
+let term2 = document.getElementById("term-2");
+let correctAnswer;
+let answerOptions = []
+
+let addition = function(){
+
+    term1.innerText = getValue(Math.pow(10, difficulty +1));
+    term2.innerText = getValue(Math.pow(10, difficulty +1));
+
+}
+
+let getValue = function(multiplier){
+    
+    return Math.floor(Math.random() * multiplier);
+
+}
 
 switch(operation){
 
     case 'addition':
-        title = "Adição";    
+        title = "Adição"; 
+        operationSymbol = '+';
+        addition();
     break;
         
     case 'subtraction':
@@ -24,3 +45,5 @@ switch(operation){
 }
 
 document.title += " " + title;
+document.getElementById("operation-symbol").innerText = operationSymbol;
+
