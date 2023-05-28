b34820f24d0323ab25ec3fb49f7ce682535cff07
f95f <95felipefernandes@gmail.com>
2023-05-27 21:10:53 -0300

    added styling

--- scripts/results.js
+++ scripts/results.js
@@ -4,11 +4,11 @@ let player = JSON.parse(transfer);
 
 let setValues = function(){
     
-    if(player.score < 25){ finalRating = '*'}
-    else if(player.score < 50){ finalRating = '**'}
-    else if(player.score < 100){ finalRating = '***'}
-    else if(player.score < 200){ finalRating = '****'}    
-    else if(player.score < 500){ finalRating = '*****'}
+    if(player.score < 25){ finalRating = '★'}
+    else if(player.score < 50){ finalRating = '★★'}
+    else if(player.score < 100){ finalRating = '★★★'}
+    else if(player.score < 200){ finalRating = '★★★★'}    
+    else if(player.score < 500){ finalRating = '★★★★★'}
     else if(player.score < 1000){ finalRating = '!'}
     else if(player.score < 10000){ finalRating = '!!'}
     else{ finalRating = '☠️'}
